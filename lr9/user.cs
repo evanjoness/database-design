f862378bc54be36549ade4a66277c3d9bd89a4d2
evanjoness <98981687+evanjoness@users.noreply.github.com>
2022-05-25 08:37:25 +0300

    Add files via upload

---
+++ lr9/user.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr9
+{
+    class User
+    {
+        public ObjectId _id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public object repositories { get; set; }
+    }
+}
